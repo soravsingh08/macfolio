b8962ee83b6976368e77cf3181002729247155ba
sorav08 <rahulrakwal3@gmail.com>
2026-01-22 23:56:11 +0530

    Add global styles and Dock component
    - Created a new SCSS file for global styles with reset and layout settings.
    - Implemented a Dock component with a footer and an icon.

---
+++ src/components/Dock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dock = () => {
+  return (
+    <div>
+        <footer>
+            <div className='icon'><img src="/mac-icons/calender.svg" alt="" /></div>
+            <h1>rauydhfsiduhfs</h1>
+        </footer>
+    </div>
+  )
+}
+
+export default Dock
